1fa400182173a03c3b66630abd15aa638bf54a12
solid73 <43271172+solid73@users.noreply.github.com>
2018-09-18 19:44:21 +0300

    update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,8 @@
 const PlatioToken = artifacts.require('PlatioToken');
 const PlatioCrowdsale = artifacts.require('PlatioCrowdsale');
 
-const wallet;
-const startTime;
-const endTime;
-const rate;
-const cap;
-const goal;
-
 module.exports = function(deployer) {
-  deployer.deploy(PlatioToken);
-  deployer.deploy(
-    PlatioCrowdsale,
-    wallet,
-    startTime,
-    endTime,
-    rate,
-    cap,
-    goal,
-    PlatioToken.address
-  );
+  deployer.deploy(PlatioToken).then(function() {
+    return deployer.deploy(PlatioCrowdsale, PlatioToken.address);
+  });
 };
